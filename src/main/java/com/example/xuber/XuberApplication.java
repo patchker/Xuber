f49c9158362ce3eaef2c7db5e576617165fb3cfe
patchker <patchker@icloud.com>
2024-07-05 12:10:14 +0200

    init commit

---
+++ src/main/java/com/example/xuber/XuberApplication.java
@@ -0,0 +1,13 @@
+package com.example.xuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xuber/XuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
